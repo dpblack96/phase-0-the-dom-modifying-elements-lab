ec72deff4a1c37d7c52f3efe99bc798c2819d94c
Dominic Black <dpblack96@gmail.com>
2022-06-07 17:41:31 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let removeMain = document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.setAttribute = 'h1#victory';
+newHeader.innerHTML = 'Dominic is the champion'
